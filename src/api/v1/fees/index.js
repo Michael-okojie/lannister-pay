c1ed4ee87247e0877bdb81e146447542decd1d95
neymarjimoh <jemohkunle2007@gmail.com>
2022-02-02 11:15:29 +0100

    fix

--- src/api/v1/fees/index.js
+++ src/api/v1/fees/index.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { setUpFeeConfiguration, computeFee, setUpFeeConfigurationJson, computeFeeJson } from "./fees_controller";
+import { setUpFeeConfigurationJson, computeFeeJson } from "./fees_controller";
 
 const router = Router();
 
@@ -7,18 +7,12 @@ const router = Router();
  * to set up fees
  * /fees - POST
  */
-router.post("/fees", setUpFeeConfiguration);
+router.post("/fees", setUpFeeConfigurationJson);
 
 /**
  * fee computation endpoint
  * /compute-transaction-fee - POST
  */
-router.post("/compute-transaction-fee", computeFee);
-
-
-// testing endpoint with fs module
-router.post("/json-fees", setUpFeeConfigurationJson)
-
-router.post("/json-compute-transaction-fee", computeFeeJson)
+router.post("/compute-transaction-fee", computeFeeJson);
 
 export default router;
